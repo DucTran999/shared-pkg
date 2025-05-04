2c856e0c80bb3cfb8695f510eacc2edfde021243
Đức Trần <tranaduc9x@gmail.com>
2025-05-04 23:45:28 +0700

    feat: add config server for timeout request

--- server/example/main.go
+++ server/example/main.go
@@ -9,7 +9,19 @@ import (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	httpServer := server.NewGinHttpServer(router, "localhost", 8000)
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	httpServer, err := server.NewGinHttpServer(router, server.ServerConfig{
+		Host: "localhost",
+		Port: 8080,
+	})
+	if err != nil {
+		log.Fatal().Msgf("initialize http server err=%v", err)
+	}
 
 	go func() {
 		if err := httpServer.Start(); err != nil {
