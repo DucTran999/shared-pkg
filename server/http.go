cb49833c72d949b7b9b63381f1e542c316ecdccc
Đức Trần <tranaduc9x@gmail.com>
2025-06-04 20:46:26 +0700

    update: new ping for checking http alive

--- server/http.go
+++ server/http.go
@@ -3,10 +3,9 @@ package server
 import (
 	"context"
 	"fmt"
+	"net"
 
 	"net/http"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -22,7 +21,24 @@ type httpServer struct {
 }
 
 func (s *httpServer) Start() error {
-	log.Info().Msgf("start server on %s", s.server.Addr)
+	go func() {
+		for {
+			// Wait for the server to be ready
+			time.Sleep(500 * time.Millisecond)
+
+			// Attempt to ping the server to check if it's ready
+			err := s.tcpPing(s.server.Addr, 5*time.Second)
+			if err != nil {
+				log.Warn().Err(err).Msgf("tcp ping to %s failed, retrying...", s.server.Addr)
+				continue
+			}
+
+			// If ping is successful, log the server address and break the loop
+			log.Info().Msgf("start server on %s", s.server.Addr)
+			break
+		}
+	}()
+
 	if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		return err
 	}
@@ -31,41 +47,20 @@ func (s *httpServer) Start() error {
 }
 
 func (s *httpServer) Stop(ctx context.Context) error {
-	log.Info().Msg("http server is shutting down...")
-
 	if err := s.server.Shutdown(ctx); err != nil {
 		return fmt.Errorf("shutdown server got err: %v", err)
 	}
 
-	log.Info().Msg("http server shutdown successfully!")
+	log.Info().Msg("http server shutdown successfully")
 	return nil
 }
 
-// GracefulShutdown handles OS signals and performs a graceful shutdown of the server.
-func GracefulShutdown(shutdownTasks ...func(ctx context.Context) error) {
-	const shutdownTimeout = 5 * time.Second
-
-	// Listen for SIGINT or SIGTERM
-	shutdownCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	<-shutdownCtx.Done()
-	log.Info().Msg("shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
-	defer cancel()
-
-	cleanExit := true
-	for _, task := range shutdownTasks {
-		if err := task(ctx); err != nil {
-			log.Warn().Err(err).Msg("shutdown task error")
-			cleanExit = false
-		}
+func (s *httpServer) tcpPing(address string, timeout time.Duration) error {
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return err
 	}
 
-	if cleanExit {
-		log.Info().Msg("server shut down cleanly")
-	} else {
-		log.Warn().Msg("server encountered errors during shutdown")
-	}
+	defer conn.Close()
+	return nil
 }
