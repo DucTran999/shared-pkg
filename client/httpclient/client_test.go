8f4d63b01cd52e90b71013bb5bd88ac12fd7e2b8
Đức Trần <tranaduc9x@gmail.com>
2025-06-06 00:10:10 +0700

    update: script coverage and add unitest http client

--- client/httpclient/client_test.go
+++ client/httpclient/client_test.go
@@ -3,24 +3,100 @@ package httpclient_test
 import (
 	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/DucTran999/shared-pkg/client/httpclient"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Get(t *testing.T) {
+func Test_NewClientValidOptions(t *testing.T) {
 	// Arrange
-	c := httpclient.NewClient()
-
-	// Act
-	resp, err := c.Get(context.Background(), "https://example.com")
+	c := httpclient.NewClient(
+		httpclient.WithIdleConnTimeout(90*time.Second),
+		httpclient.WithMaxIdleConns(50),
+		httpclient.WithTimeout(5*time.Second),
+		httpclient.WithMaxIdleConnsPerHost(10),
+		httpclient.WithTLSHandshakeTimeout(5*time.Second),
+	)
 
 	// Assert
-	require.NoError(t, err, "Expected no error when do request to example.com")
-	require.NotNil(t, resp, "Expected non-empty response from example.com")
-	require.Equal(t, http.StatusOK, resp.StatusCode, "Expected status code 200 OK")
-	require.Equal(t, "200 OK", resp.Status, "Expected status text to match 200 OK")
-	assert.NotEmpty(t, resp.Body, "Expected non-empty response body from example.com")
+	require.NotNil(t, c, "Expected non-nil httpClient instance")
+}
+
+func Test_NewClientInvalidOptions(t *testing.T) {
+	// Arrange negative or zero values for options
+	c := httpclient.NewClient(
+		httpclient.WithIdleConnTimeout(-90*time.Second),
+		httpclient.WithMaxIdleConns(-50),
+		httpclient.WithTimeout(-5*time.Second),
+		httpclient.WithMaxIdleConnsPerHost(-10),
+		httpclient.WithTLSHandshakeTimeout(-5*time.Second),
+	)
+
+	// Assert
+	require.NotNil(t, c, "Expected non-nil httpClient instance")
+}
+
+func Test_HttpClientGet(t *testing.T) {
+	t.Run("returns response on successful request", func(t *testing.T) {
+		// Create a mock server
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, "/test", r.URL.Path)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"message":"success"}`))
+		}))
+		defer mockServer.Close()
+
+		// Initialize your HTTP client
+		httpClient := httpclient.NewClient(
+			httpclient.WithTimeout(2 * time.Second),
+		)
+
+		// Call your method
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		resp, err := httpClient.Get(ctx, mockServer.URL+"/test")
+		require.NoError(t, err)
+		require.JSONEq(t, `{"message":"success"}`, string(resp.Body))
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("returns error on non-200 response", func(t *testing.T) {
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, "/test", r.URL.Path)
+			http.Error(w, "internal error", http.StatusInternalServerError)
+		}))
+		defer mockServer.Close()
+
+		// Initialize your HTTP client
+		client := httpclient.NewClient(
+			httpclient.WithTimeout(2 * time.Second),
+		)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		resp, err := client.Get(ctx, mockServer.URL+"/test")
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	})
+
+	t.Run("unreachable host returns error", func(t *testing.T) {
+		// Initialize your HTTP client
+		client := httpclient.NewClient(
+			httpclient.WithTimeout(2 * time.Second),
+		)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*100)
+		defer cancel()
+
+		// non-routable IP to simulate connection failure
+		_, err := client.Get(ctx, "http://10.255.255.1:12345")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "failed to do request")
+	})
 }
--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -56,7 +57,7 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
@@ -65,11 +66,11 @@ require (
 	go.opentelemetry.io/otel/trace v1.26.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/net v0.37.0 // indirect
-	golang.org/x/sync v0.12.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
